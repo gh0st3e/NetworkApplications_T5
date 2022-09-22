f8341b226a5c25ff750392bd86a63cdb667cd3ea
gh0st3e <bigdenbstu@yandex.by>
2022-09-22 20:30:45 +0300

    added lab2 (Sockets)

---
+++ Lab_2MS/Client/Client/Client.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include "Winsock2.h"			   // Заголовок WS2_32.dll
+#include "WS2tcpip.h"
+
+#pragma comment(lib, "WS2_32.lib") // экспорт WS2_32.dll
+
+using namespace std;
+
+string GetErrorMsgText(int code);
+string SetErrorMsgText(string msgText, int code);
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	try {
+		WSADATA wsaData;
+		SOCKET clientSocket; // клиентский сокет
+
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)                   // инициализация библиотеки
+			throw  SetErrorMsgText("Startup:", WSAGetLastError());
+
+		if ((clientSocket = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET) // создать сокет
+			throw  SetErrorMsgText("socket:", WSAGetLastError());
+		
+		SOCKET serverSocket;
+
+		SOCKADDR_IN serv;                    // параметры  сокета сервера
+		serv.sin_family = AF_INET;           // используется IP-адресация  
+		serv.sin_port = htons(2000);                   // TCP-порт 2000
+		inet_pton(AF_INET, "127.0.0.1", &(serv.sin_addr)); // адрес сервера
+
+		//serv.sin_addr.s_addr = inet_addr("127.0.0.1");  // адрес сервера
+
+		if ((connect(clientSocket, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR)
+			throw  SetErrorMsgText("connect:", WSAGetLastError());
+
+		
+		char ibuf[50],                     //буфер ввода 
+			obuf[50] = "Hello from Client ";  //буфер вывода
+		int  libuf = 0,                    //количество принятых байт
+			lobuf = 0;                    //количество отправленных байь 
+
+		cout << "Введите кол-во сообщений: ";
+		int count;
+		cin >> count;
+
+		for (int i = 0; i < count; i++) {
+			if ((lobuf = send(clientSocket, obuf, strlen(obuf) + 1, NULL)) == SOCKET_ERROR)
+				throw  SetErrorMsgText("send:", WSAGetLastError());
+
+			if ((libuf = recv(clientSocket, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR)
+				throw  SetErrorMsgText("recv:", WSAGetLastError());
+
+			cout << " Сообщение от сервера " << ": " << ibuf << (i + 1) << endl;
+		}
+		if (SOCKET_ERROR == send(clientSocket, "Конец передачи", sizeof("Конец передачи"), NULL))
+		{
+			cout << "send exit : " << GetLastError() << endl;
+		}
+
+
+		
+
+
+
+		if (closesocket(clientSocket) == SOCKET_ERROR)							// закрыть сокет
+			throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+
+		if (WSACleanup() == SOCKET_ERROR)							    // завершить работу с библиотекой
+			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+
+
+
+
+
+	}
+	catch (string errorMsgText) {
+		{ cout << endl << "WSAGetLastError: " << errorMsgText; }
+	}
+
+	return 0;
+}
+
+string GetErrorMsgText(int code) // Функция позволяет получить сообщение ошибки
+{
+	string msgText;
+	switch (code)
+	{
+	case WSAEINTR: msgText = "Работа функции прервана";
+		break;
+	case WSAEACCES: msgText = "Разрешение отвергнуто";
+		break;
+	case WSAEFAULT: msgText = "Ошибочный адрес";
+		break;
+	case WSASYSCALLFAILURE: msgText = "Аварийное завершение системного вызова";
+		break;
+	default: msgText = "***ERROR***";
+		break;
+	}
+	//TODO: добавить коды
+	return msgText;
+}
+
+
+string SetErrorMsgText(string msgText, int code) // Функция возвращает сообщение ошибки
+{
+	return msgText + GetErrorMsgText(code);
+}
---
+++ Lab_2MS/Server/Server/Server.cpp
@@ -0,0 +1,129 @@
+﻿#include <iostream>
+#include "Winsock2.h"			   // Заголовок WS2_32.dll
+
+#pragma comment(lib, "WS2_32.lib") // экспорт WS2_32.dll
+#pragma warning(suppress : 4996)
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+string GetErrorMsgText(int code);
+string SetErrorMsgText(string msgText, int code);
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int i;
+	int t;
+	try {
+		WSADATA wsaData;
+		SOCKET serverSocket; // серверный сокет
+
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)                   // инициализация библиотеки
+			throw  SetErrorMsgText("Startup:", WSAGetLastError());
+
+		if ((serverSocket = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET) // создать сокет
+			throw  SetErrorMsgText("socket:", WSAGetLastError());
+
+		SOCKADDR_IN serv;				   // параметры сокета sS
+		serv.sin_family = AF_INET;		   // используется IP-адресация
+		serv.sin_port = htons(2000);	   // порт 2000
+		serv.sin_addr.s_addr = INADDR_ANY; // любой собственный IP - адрес
+
+		if (bind(serverSocket, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR) // установка параметров serv для сокета serverSocket
+			throw  SetErrorMsgText("bind:", WSAGetLastError());
+
+		if (SOCKET_ERROR == listen(serverSocket, 2)) // установка сокета сервера в режим прослушивания (2 параметр - макс. длина очереди)
+		{
+			cout << "Listen: " << WSAGetLastError << endl;
+		}
+		cout << "Сервер начал прослушку!" << endl;
+
+		SOCKET clientSocket;	                 // сокет для обмена данными с клиентом 
+		SOCKADDR_IN clnt;						 // параметры  сокета клиента
+		memset(&clnt, 0, sizeof(clnt));			 // обнулить память
+		int lclnt = sizeof(clnt);			     // размер SOCKADDR_IN
+
+		while (true) {
+			i = 1;
+			// Функция accept создает канал на стороне сервера и иоздает сокет для обмена данными по этому каналу
+			if ((clientSocket = accept(serverSocket, (sockaddr*)&clnt, &lclnt)) == INVALID_SOCKET)
+				throw  SetErrorMsgText("accept:", WSAGetLastError());
+
+			cout << "Соединение с клиентом успешно!" << endl;
+			cout << "Адрес клиента: " << inet_ntoa(clnt.sin_addr) << ":" << htons(clnt.sin_port) << "\n\n\n\n\n";
+
+			char ibuf[50],                     //буфер ввода 
+				obuf[50] = "sever: принято ";  //буфер вывода
+			int  libuf = 0,                    //количество принятых байт
+				lobuf = 0;
+
+			while (true) {
+				
+				t = clock();
+
+				if ((libuf = recv(clientSocket, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR)
+				{
+					cout << "Recv: " << WSAGetLastError() << endl;
+					break;
+				}
+
+
+				_itoa(lobuf, obuf + sizeof("sever: принято ") - 1, 10);
+
+				cout << "Принято сообщение " << i << ": " << ibuf << endl;
+				i++;
+
+				if ((lobuf = send(clientSocket, ibuf, strlen(ibuf) + 1, NULL)) == SOCKET_ERROR)
+				{
+					cout << "Send: " << WSAGetLastError() << endl;
+					break;
+				}
+
+
+
+			}
+
+			cout << "\n\nКлиент отключился" << endl;
+			cout << "Затраченное время: " << ((float)t) / CLOCKS_PER_SEC << " секунд";
+		}
+		if (closesocket(serverSocket) == SOCKET_ERROR)							// закрыть сокет
+			throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+
+		if (WSACleanup() == SOCKET_ERROR)							    // завершить работу с библиотекой
+			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+
+		
+	}
+	catch (string errorMsgText) {
+		{ cout << endl << "WSAGetLastError: " << errorMsgText; }
+	}
+	return 0;
+}
+
+string GetErrorMsgText(int code) // Функция позволяет получить сообщение ошибки
+{
+    string msgText;
+    switch (code)
+    {
+	case WSAEINTR: msgText = "Работа функции прервана";
+		break;
+	case WSAEACCES: msgText = "Разрешение отвергнуто";
+		break;
+	case WSAEFAULT: msgText = "Ошибочный адрес";
+		break;
+	case WSASYSCALLFAILURE: msgText = "Аварийное завершение системного вызова";
+		break;
+	default: msgText = "***ERROR***";
+		break;
+	}
+	//TODO: добавить коды
+	return msgText;
+}
+
+
+string SetErrorMsgText(string msgText, int code) // Функция возвращает сообщение ошибки
+{
+	return msgText + GetErrorMsgText(code);
+}
