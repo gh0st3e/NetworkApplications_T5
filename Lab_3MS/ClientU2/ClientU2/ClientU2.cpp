dce601bb25d31511cbf55bc9a82ee7e9aecc7b90
gh0st3e <bigdenbstu@yandex.by>
2022-09-28 12:10:02 +0300

    Any ip client

---
+++ Lab_3MS/ClientU2/ClientU2/ClientU2.cpp
@@ -0,0 +1,177 @@
+﻿#include <iostream>
+#include <charconv>
+#include "Winsock2.h"			   // Заголовок WS2_32.dll
+#include "WS2tcpip.h"
+
+#pragma comment(lib, "WS2_32.lib") // экспорт WS2_32.dll
+
+using namespace std;
+
+string GetErrorMsgText(int code);
+string SetErrorMsgText(string msgText, int code);
+char* getID();
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	try {
+		WSADATA wsaData;
+		SOCKET clientSocket; // клиентский сокет
+
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)                   // инициализация библиотеки
+			throw  SetErrorMsgText("Startup:", WSAGetLastError());
+
+		if ((clientSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET) // создать сокет
+			throw  SetErrorMsgText("socket:", WSAGetLastError());
+
+		SOCKET serverSocket;
+
+		SOCKADDR_IN serv;                    // параметры  сокета сервера
+		int lc = sizeof(serv);
+		serv.sin_family = AF_INET;           // используется IP-адресация  
+		serv.sin_port = htons(2000);                   // UDP-порт 2000
+		inet_pton(AF_INET, getID(), &(serv.sin_addr)); // адрес сервера
+
+		//serv.sin_addr.s_addr = inet_addr("127.0.0.1");  // адрес сервера
+
+		if ((connect(clientSocket, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR)
+			throw  SetErrorMsgText("connect:", WSAGetLastError());
+
+
+		char ibuf[50],                     //буфер ввода 
+			obuf[50] = "Hello from ClientU";  //буфер вывода
+		int  libuf = 0,                    //количество принятых байт
+			lobuf = 0;                    //количество отправленных байь 
+
+
+		cout << "Введите кол-во сообщений: ";
+		int count;
+		cin >> count;
+
+		for (int i = 0; i < count; i++) {
+			
+			
+
+			if ((lobuf = sendto(clientSocket, obuf, strlen(obuf) + 1, NULL, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR) // отправить сообщение
+				throw  SetErrorMsgText("send:", WSAGetLastError());
+			
+
+			if ((libuf = recvfrom(clientSocket, ibuf, sizeof(ibuf), NULL, (sockaddr*)&serv, &lc)) == SOCKET_ERROR)
+				throw  SetErrorMsgText("recv:", WSAGetLastError());
+
+			cout << " Сообщение от сервера " << ": " << ibuf << " " << (i + 1) << endl;
+		}
+
+
+
+
+		if (closesocket(clientSocket) == SOCKET_ERROR)							// закрыть сокет
+			throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+
+		if (WSACleanup() == SOCKET_ERROR)							    // завершить работу с библиотекой
+			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+
+
+
+
+
+	}
+	catch (string errorMsgText) {
+		{ cout << endl << "WSAGetLastError: " << errorMsgText; }
+	}
+
+	return 0;
+}
+
+char* getID()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	wVersionRequested = MAKEWORD(1, 0);
+	int err = WSAStartup(wVersionRequested, &wsaData);
+	if (err == 0)
+	{
+		char hn[1024];
+		struct hostent* adr;
+		if (gethostname((char*)&hn, 1024))
+		{
+			int err = WSAGetLastError();
+			
+		};
+		adr = gethostbyname(hn);
+		if (adr) {
+			char* LocalIp = new char[15];
+			sprintf(LocalIp, "%d.%d.%d.%d",
+				(unsigned char)adr->h_addr_list[0][0],
+				(unsigned char)adr->h_addr_list[0][1],
+				(unsigned char)adr->h_addr_list[0][2],
+				(unsigned char)adr->h_addr_list[0][3]);
+			return LocalIp;
+		}
+	}
+}
+
+string GetErrorMsgText(int code) // Функция позволяет получить сообщение ошибки
+{
+	switch (code)                      // check return code   
+	{
+	case WSAEINTR: return "WSAEINTR: Работа функции прервана ";
+	case WSAEACCES: return "WSAEACCES: Разрешение отвергнуто";
+	case WSAEFAULT:	return "WSAEFAULT: Ошибочный адрес";
+	case WSAEINVAL:	return "WSAEINVAL: Ошибка в аргументе";
+	case WSAEMFILE: return "WSAEMFILE: Слишком много файлов открыто";
+	case WSAEWOULDBLOCK: return "WSAEWOULDBLOCK: Ресурс временно недоступен";
+	case WSAEINPROGRESS: return "WSAEINPROGRESS: Операция в процессе развития";
+	case WSAEALREADY: return "WSAEALREADY: Операция уже выполняется";
+	case WSAENOTSOCK: return "WSAENOTSOCK: Сокет задан неправильно";
+	case WSAEDESTADDRREQ: return "WSAEDESTADDRREQ: Требуется адрес расположения";
+	case WSAEMSGSIZE: return "WSAEMSGSIZE: Сообщение слишком длинное";
+	case WSAEPROTOTYPE: return "WSAEPROTOTYPE: Неправильный тип протокола для сокета";
+	case WSAENOPROTOOPT: return "WSAENOPROTOOPT: Ошибка в опции протокола";
+	case WSAEPROTONOSUPPORT: return "WSAEPROTONOSUPPORT: Протокол не поддерживается";
+	case WSAESOCKTNOSUPPORT: return "WSAESOCKTNOSUPPORT: Тип сокета не поддерживается";
+	case WSAEOPNOTSUPP:	return "WSAEOPNOTSUPP: Операция не поддерживается";
+	case WSAEPFNOSUPPORT: return "WSAEPFNOSUPPORT: Тип протоколов не поддерживается";
+	case WSAEAFNOSUPPORT: return "WSAEAFNOSUPPORT: Тип адресов не поддерживается протоколом";
+	case WSAEADDRINUSE:	return "WSAEADDRINUSE: Адрес уже используется";
+	case WSAEADDRNOTAVAIL: return "WSAEADDRNOTAVAIL: Запрошенный адрес не может быть использован";
+	case WSAENETDOWN: return "WSAENETDOWN: Сеть отключена";
+	case WSAENETUNREACH: return "WSAENETUNREACH: Сеть не достижима";
+	case WSAENETRESET: return "WSAENETRESET: Сеть разорвала соединение";
+	case WSAECONNABORTED: return "WSAECONNABORTED: Программный отказ связи";
+	case WSAECONNRESET:	return "WSAECONNRESET: Связь восстановлена";
+	case WSAENOBUFS: return "WSAENOBUFS: Не хватает памяти для буферов";
+	case WSAEISCONN: return "WSAEISCONN: Сокет уже подключен";
+	case WSAENOTCONN: return "WSAENOTCONN: Сокет не подключен";
+	case WSAESHUTDOWN: return "WSAESHUTDOWN: Нельзя выполнить send : сокет завершил работу";
+	case WSAETIMEDOUT: return "WSAETIMEDOUT: Закончился отведенный интервал  времени";
+	case WSAECONNREFUSED: return "WSAECONNREFUSED: Соединение отклонено";
+	case WSAEHOSTDOWN: return "WSAEHOSTDOWN: Хост в неработоспособном состоянии";
+	case WSAEHOSTUNREACH: return "WSAEHOSTUNREACH: Нет маршрута для хоста";
+	case WSAEPROCLIM: return "WSAEPROCLIM: Слишком много процессов";
+	case WSASYSNOTREADY: return "WSASYSNOTREADY: Сеть не доступна";
+	case WSAVERNOTSUPPORTED: return "WSAVERNOTSUPPORTED: Данная версия недоступна";
+	case WSANOTINITIALISED:	return "WSANOTINITIALISED: Не выполнена инициализация WS2_32.DLL";
+	case WSAEDISCON: return "WSAEDISCON: Выполняется отключение";
+	case WSATYPE_NOT_FOUND: return "WSATYPE_NOT_FOUND: Класс не найден";
+	case WSAHOST_NOT_FOUND:	return "WSAHOST_NOT_FOUND: Хост не найден";
+	case WSATRY_AGAIN: return "WSATRY_AGAIN: Неавторизированный хост не найден";
+	case WSANO_RECOVERY: return "WSANO_RECOVERY: Неопределенная ошибка";
+	case WSANO_DATA: return "WSANO_DATA: Нет записи запрошенного типа";
+	case WSA_INVALID_HANDLE: return "WSA_INVALID_HANDLE: Указанный дескриптор события с ошибкой";
+	case WSA_INVALID_PARAMETER: return "WSA_INVALID_PARAMETER: Один или более параметров с ошибкой";
+	case WSA_IO_INCOMPLETE:	return "WSA_IO_INCOMPLETE: Объект ввода - вывода не в сигнальном состоянии";
+	case WSA_IO_PENDING: return "WSA_IO_PENDING: Операция завершится позже";
+	case WSA_NOT_ENOUGH_MEMORY: return "WSA_NOT_ENOUGH_MEMORY: Не достаточно памяти";
+	case WSA_OPERATION_ABORTED: return "WSA_OPERATION_ABORTED: Операция отвергнута";
+	case WSASYSCALLFAILURE: return "WSASYSCALLFAILURE: Аварийное завершение системного вызова";
+	default: return "**ERROR**";
+	};
+}
+
+
+string SetErrorMsgText(string msgText, int code) // Функция возвращает сообщение ошибки
+{
+	return msgText + GetErrorMsgText(code);
+}
