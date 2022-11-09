83897fe159aa10e71b142761483b64d62c36d0f5
gh0st3e <bigdenbstu@yandex.by>
2022-11-09 11:43:52 +0300

    added lab7 (Mailslot)

---
+++ Lab_7MS/ClientMS/ClientMS.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <string>
+#include "ClientMS.h"
+
+using namespace std;
+
+string GetErrorMail(int code)
+{
+	string msgText = "";
+	switch (code)
+	{
+	case WSAEINTR: msgText = "WSAEINTR"; break;
+	case WSAEACCES:	msgText = "WSAEACCES"; break;
+	case WSAEFAULT:	msgText = "WSAEFAULT"; break;
+	default: msgText = "Error";
+	};
+	return msgText;
+}
+
+string SetErrorMail(string msgText, int code)
+{
+	return msgText + GetErrorMail(code);
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	try {
+		HANDLE clientMailSlot;
+		double t1, t2;
+
+		if ((clientMailSlot = CreateFile(L"\\\\.\\mailslot\\BOX",
+			GENERIC_WRITE,
+			FILE_SHARE_READ | FILE_SHARE_WRITE,
+			NULL,
+			OPEN_EXISTING,
+			NULL,
+			NULL)) == INVALID_HANDLE_VALUE)
+			throw SetErrorMail("CreateFile: ", GetLastError());
+
+		cout << "Hello i am Client" << endl;
+
+		char writeBuf[50] = "Hello from Client-Mailslot";
+		DWORD writeMsg;
+
+		t1 = clock();
+
+		for (int i = 1; i <= 1000; i++) {
+			if(!WriteFile(clientMailSlot,writeBuf,sizeof(writeBuf),&writeMsg,NULL))
+				throw SetErrorMail("WriteFile: ", GetLastError());
+
+			cout << "Message " << i << " was sent" << endl;
+		}
+
+		t2 = clock();
+
+		if (!CloseHandle(clientMailSlot))
+			throw "Error: CloseHandle";
+
+		cout << endl << "Время передачи: " << (t2 - t1) / 1000 << " сек." << endl << endl;
+		system("pause");
+	}
+	catch (string e) {
+		cout << e << endl;
+	}
+	
+	return 0;
+}
---
+++ Lab_7MS/ServerMS/ServerMS.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <string>
+
+using namespace std;
+
+string GetErrorMail(int code)
+{
+	string msgText = "";
+	switch (code)
+	{
+	case WSAEINTR: msgText = "WSAEINTR"; break;
+	case WSAEACCES:	msgText = "WSAEACCES"; break;
+	case WSAEFAULT:	msgText = "WSAEFAULT"; break;
+	default: msgText = "Error";
+	};
+	return msgText;
+}
+
+string SetErrorMail(string msgText, int code)
+{
+	return msgText + GetErrorMail(code);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	try {
+		HANDLE serverMailSlot;
+		int i = 0;
+		double t1, t2;
+
+		if ((serverMailSlot = CreateMailslot(L"\\\\.\\mailslot\\BOX",
+			300,
+			MAILSLOT_WAIT_FOREVER,
+			NULL)) == INVALID_HANDLE_VALUE)
+			throw SetErrorMail("CreateMailslot", GetLastError());
+
+		cout << "Hello i am server" << endl;
+
+		char readBuf[50];
+		DWORD readMsg;
+		do {
+			i++;
+			if (!ReadFile(serverMailSlot, readBuf, sizeof(readBuf), &readMsg, NULL))
+				throw SetErrorMail("ReadFile", GetLastError());
+			SetMailslotInfo(serverMailSlot, 5000);
+			if (i == 1)
+				t1 = clock();
+			cout << readBuf << " " << i << endl;
+			
+		} while (strcmp(readBuf, " ") != 0);
+
+		t2 = clock();
+
+		cout << "Время передачи: " << (t2 - t1) / 1000 << " сек." << endl;
+		cout << "Количество сообщений: " << i - 1 << endl << endl;
+
+		if (!CloseHandle(serverMailSlot))
+			throw "Error: CloseHandle";
+
+		system("pause");
+	}
+	catch (string e) {
+		if (e == "ReadFileError") {
+			cout << e << " Timeout";
+			return 0;
+		}
+		cout << e << endl;
+	}
+	return 0;
+}
